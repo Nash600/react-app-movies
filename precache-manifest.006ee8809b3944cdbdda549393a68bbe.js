22b8bdd601a12bddb6fc12953d23806d0fce4cd1
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-07-10 17:21:32 +0300

    Updates

---
+++ precache-manifest.006ee8809b3944cdbdda549393a68bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f343f7262e52a7e8c1dc",
+    "url": "/react-app-movies/static/css/main.9805f3be.chunk.css"
+  },
+  {
+    "revision": "f343f7262e52a7e8c1dc",
+    "url": "/react-app-movies/static/js/main.f343f726.chunk.js"
+  },
+  {
+    "revision": "90c23a584280c5dbc6d9",
+    "url": "/react-app-movies/static/js/runtime~main.90c23a58.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/react-app-movies/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "1abb9f0b21ddf2c473d7b056152e89ce",
+    "url": "/react-app-movies/index.html"
+  }
+];
